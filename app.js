2182e11a0d6458d8c9b58b7a48c0c44a17fbc926
Kevin Do <kevind0501@gmail.com>
2021-08-26 11:31:02 -0700

    Created UI for add

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 // import functions
+import { add } from './calculator.js';
 
 // reference needed DOM elements
-
+const addNumber1 = document.getElementById('add-number-1');
+const addNumber2 = document.getElementById('add-number-2');
+const addButton = document.getElementById('add-button');
+const addAnswer = document.getElementById('add-answer');
 
 // set event listeners 
+addButton.addEventListener('click', () => {
+
     // get user input(s)
+    const num1 = Number(addNumber1.value);
+    const num2 = Number(addNumber2.value);
+
     // do any needed work with the value(s)
+    const sum = add(num1, num2);
     // update DOM to reflect new value(s)
+    addAnswer.textContent = sum;
+});
--- test/tests.js
+++ test/tests.js
@@ -1,17 +1,20 @@
 // IMPORT MODULES under test here:
-// import { add } from '../calculator.js';
+import { add } from '../calculator.js';
 
 const test = QUnit.test;
 
+
 // name your test by what it is testing
-test('time to test a function', (expect) => {
+test('add two numbers', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const x = 1;
+    const y = 3;
+    const expected = 4;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true; // use your function here
+    const actual = add(x, y); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
