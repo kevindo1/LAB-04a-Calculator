fb5cdfa87eba1640f161695d5f933c36b4131ca1
Kevin Do <kevind0501@gmail.com>
2021-08-26 13:25:09 -0700

    subtract, multiply, divide functions added

--- app.js
+++ app.js
@@ -19,3 +19,55 @@ addButton.addEventListener('click', () => {
     // update DOM to reflect new value(s)
     addAnswer.textContent = sum;
 });
+
+//import 
+import {subtract} from './calculator.js';
+
+//DOM elements
+const subtractNumber1 = document.getElementById('subtract-number-1');
+const subtractNumber2 = document.getElementById('subtract-number-2');
+const subtractButton = document.getElementById('subtract-button');
+const subtractAnswer = document.getElementById('subtract-answer');
+
+//event listeners
+
+subtractButton.addEventListener('click', () => {
+    const num1 = Number(subtractNumber1.value);
+    const num2 = Number(subtractNumber2.value);
+
+    const sum = subtract(num1, num2);
+    
+    subtractAnswer.textContent = sum;
+});
+
+import { multiply } from './calculator.js';
+
+const multiplyNumber1 = document.getElementById('multiply-number-1');
+const multiplyNumber2 = document.getElementById('multiply-number-2');
+const multiplyButton = document.getElementById('multiply-button');
+const multiplyAnswer = document.getElementById('multiply-answer'); 
+
+multiplyButton.addEventListener('click', () => {
+    const num1 = Number(multiplyNumber1.value);
+    const num2 = Number(multiplyNumber2.value);
+
+    const sum = multiply(num1, num2);
+
+    multiplyAnswer.textContent = sum;
+});
+
+import { divide } from './calculator.js';
+
+const divideNumber1 = document.getElementById('divide-number-1');
+const divideNumber2 = document.getElementById('divide-number-2');
+const divideButton = document.getElementById('divide-button');
+const divideAnswer = document.getElementById('divide-answer'); 
+
+divideButton.addEventListener('click', () => {
+    const num1 = Number(divideNumber1.value);
+    const num2 = Number(divideNumber2.value);
+
+    const sum = divide(num1, num2);
+
+    divideAnswer.textContent = sum;
+});
--- test/tests.js
+++ test/tests.js
@@ -1,20 +1,21 @@
 // IMPORT MODULES under test here:
-import { add } from '../calculator.js';
+// import { add } from '../calculator.js';
+import { subtract } from '../calculator.js';
 
 const test = QUnit.test;
 
 
 // name your test by what it is testing
-test('add two numbers', (expect) => {
+test('subtract two numbers', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const x = 1;
-    const y = 3;
-    const expected = 4;
+    const x = 3;
+    const y = 1;
+    const expected = 2;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = add(x, y); // use your function here
+    const actual = subtract(x, y); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
